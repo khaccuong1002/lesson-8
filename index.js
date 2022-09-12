1130e575b22551bec7732f488ef8c3def689b321
cuongtk1002 <tkcuong1002@gmail.com>
2022-09-12 15:46:43 +0700

    if-else isstructure

--- index.js
+++ index.js
@@ -12,3 +12,25 @@ if (year % 4 == 0) {
     } else {
         alert(year + " is NOT a leap year");
     }
+
+
+
+
+let year = parseInt(prompt("Enter a year"));
+let isLeapYear = false;
+
+if (year % 4 == 0) {
+    if (year % 100 == 0) {
+        if (year % 400 == 0) {
+            isLeapYear = true;
+        }
+    } else {
+        isLeapYear = true;
+    }
+}
+
+if (isLeapYear) {
+    alert(year + " is a leap year");
+} else {
+    alert(year + " is NOT a leap year");
+}
